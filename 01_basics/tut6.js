2c913d92c91d1fd5ece9684b838f7a064ad13788
Nitin0069 <nitin4rana@gmail.com>
2026-07-24 18:12:37 +0530

    strings

---
+++ 01_basics/tut6.js
@@ -0,0 +1,20 @@
+const name = "nitin";
+const repoCount = 50;
+
+   console.log(name + repoCount + "Value");
+
+   console.log(`My name is ${name} and repocount is ${repoCount} itna jyada. `);
+ 
+
+   let x = 5;
+   console.log(` the square of ${x} is ${x * x}`);
+
+
+   console.log(`hello
+    world`);
+
+    const gameName = new String("Ntitn");
+    console.log(gameName[2]);
+    console.log(gameName.__proto__);
+    console.log(gameName.length);
+    
