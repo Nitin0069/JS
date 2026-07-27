d815333e39597e28544036d75e2abc5a4e6f30f1
Nitin0069 <nitin4rana@gmail.com>
2026-07-27 21:27:02 +0530

    array basics

---
+++ 02_basics/tut1.js
@@ -0,0 +1,27 @@
+
+
+
+const Arr2 = new Array(0,1,2,3,4,5, true, "hitesh");
+console.log(Arr2.length);
+
+
+//ARRAY methods
+const myArr = [0, 1, 2, 3]
+myArr.push(4,5);
+myArr.pop();
+
+
+myArr.unshift(-1,-2);
+myArr.shift();
+console.log(myArr);
+console.log(myArr.includes(-7));
+console.log(myArr.indexOf(12));
+
+const newArr = myArr.join();
+console.log(myArr);
+console.log(newArr);
+console.log(typeof myArr);
+console.log(typeof newArr);
+
+
+
