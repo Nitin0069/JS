694cbd0eb255e3ed55caee1e01e6a74134e3dc8b
Nitin0069 <nitin4rana@gmail.com>
2026-07-28 19:58:56 +0530

    object copy

---
+++ 02_basics/tut4.js
@@ -0,0 +1,34 @@
+//const tinderUser = new Object ();
+const tinderUser = {}
+tinderUser.id = "123abc";
+tinderUser.name = "sammy";
+tinderUser.isLoggedIn = "false";
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname: "Hitesh",
+            lastname: "choudhary",
+        }
+    }
+};
+//console.log(regularUser.userfullname.firstname);
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+//const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+
+
