f8570de68b37d2b6bc23c4e8e2401823971dd06f
Nitin0069 <nitin4rana@gmail.com>
2026-07-31 21:00:09 +0530

    function practise

--- 02_basics/tut6.js
+++ 02_basics/tut6.js
@@ -47,4 +47,76 @@
    //console.log(returnSecondValue(myNewArray));
       console.log(returnSecondValue([200,300,400,500]));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      function sayHello () {
+        console.log("Hello World");
+      }
+      sayHello();
+
+
+      function add (num1, num2) {
+        return num1 + num2;
+      }
+      
+     // add (6,3);
+      console.log(add(6,3));
+
+
+
+      function login (username) {
+        if (!username) {
+         return ` ${username} just logged in `;
+      }
+    }
+      console.log(login("hitesh"));
+
+
+      const userrr = {
+        username: "Nitin",
+        pricee: 199
+      };
+      function handle (any) {
+        console.log(any.username);
+        console.log(any.pricee);
+      }
+      handle(userrr);
+
+
+      const myyoo = [100,200,300,400]
+      function returnu(gett) {
+        return gett[0]
+      }
+      console.log(returnu(myyoo));
+
+
+
+
+
  
