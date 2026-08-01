86b37c4c8a4749b834e5fa1ea0f0048e132dc7cf
Nitin0069 <nitin4rana@gmail.com>
2026-08-01 17:03:08 +0530

    hoisting

--- 03_basics/tut3.js
+++ 03_basics/tut3.js
@@ -88,5 +88,14 @@ if (true) {
 
 
 
+console.log(addone (5));
 
- 
+ function addone (num) {
+    return num + 1 
+ }
+
+
+console.log(addTwo(5))
+var addTwo = function (num) {
+    return num + 2
+}
