78f8549c8879f9cd603956f89c71bce7478064bf
Nitin0069 <nitin4rana@gmail.com>
2026-08-04 16:38:24 +0530

    nested if

--- 04_basics/tut1.js
+++ 04_basics/tut1.js
@@ -45,5 +45,40 @@ const temperature = 50;
  */
 
 
-        const balance = 1000
-        if (balance > 500) console.log("chl");
+      //  const balance = 1000
+      ///  if (balance > 500) console.log("chl");
+           
+
+
+      /* const balance = 1000;
+      if(balance < 500) {
+        console.log("less than 500");
+      }
+      else if (balance < 750) {
+        console.log("less than 750");
+      }
+      else if (balance < 1000) {
+        console.log("equal to 1000") 
+      }
+      else {
+        console.log("more than 1000")
+      }   */
+
+
+        let age = 20;
+        let hasLicense = "nunu";
+        if(age >= 28) {
+            console.log("jaa bhadwe");
+            if(hasLicense == "nunnu") {
+                console.log("jaa bhadwe 2");
+            }
+        }
+                  if(age >= 18) {
+            console.log("ajaa bhadwe");
+            if(hasLicense == "nuunu") {
+                console.log("ajaa bhadwe 2");
+            }
+        }
+
+            
+        
