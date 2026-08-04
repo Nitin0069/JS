2f4042c414909f5e7a38b2f2a6365f327fe0befd
Nitin0069 <nitin4rana@gmail.com>
2026-08-04 15:56:12 +0530

    if and if else

---
+++ 04_basics/tut1.js
@@ -0,0 +1,49 @@
+ // if 
+
+
+ /*let name = "Nitin";
+
+ if (name === "Nitinn") {
+console.log("niitin");
+ }
+ */
+
+
+ //operator  < , > <= , >=, ==, === , != 
+/*
+ let name = "Rohit";
+
+if (name != "Rohit") {
+    console.log("aja");
+}
+else {
+    console.log("htt");
+}
+
+if (3!=2) {
+    console.log("yo")
+    }
+
+
+const temperature = 50;
+    if (temperature < 48 ) {
+        console.log("true")
+    }
+    else {
+        console.log(false)
+    }
+    
+    */
+
+ /*   const score = 200;
+    if(score > 100 ) {
+        var power = "fly"
+        console.log(`User power: ${power} `);
+        console.log(power);
+    }
+        console.log(`User power: ${power} `);
+ */
+
+
+        const balance = 1000
+        if (balance > 500) console.log("chl");
