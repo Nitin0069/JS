43f646b3b0748782e0a5b1ec958b7ed225b6ec55
Nitin0069 <nitin4rana@gmail.com>
2026-08-05 17:40:05 +0530

    for loop 5 ka table

---
+++ 04_basics/tut5.js
@@ -0,0 +1,19 @@
+ // loop;
+ // for
+
+
+/*for (let index = 0; index < 10; index++ ) {
+    const element = index;
+    console.log(element);
+}
+*/
+console.log("Table of 5 is");
+for(let i = 1; i <= 10; i++) {
+   
+
+//console.log(` 5 * ${i} = ${i * 5} ` );
+console.log("5 * " + (i) + "=" + (5 * i));
+ if( i == 5) {
+        break;
+}
+}
