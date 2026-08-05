6f85fdd0b0613dfdb05481a799c70806c507e7ce
Nitin0069 <nitin4rana@gmail.com>
2026-08-05 19:03:49 +0530

    nested finish

--- 04_basics/tut5.js
+++ 04_basics/tut5.js
@@ -7,13 +7,49 @@
     console.log(element);
 }
 */
-console.log("Table of 5 is");
-for(let i = 1; i <= 10; i++) {
+//console.log("Table of 5 is");
+//for(let i = 1; i <= 10; i++) {
    
 
-//console.log(` 5 * ${i} = ${i * 5} ` );
-console.log("5 * " + (i) + "=" + (5 * i));
+  //console.log(` 5 * ${i} = ${i * 5} ` );
+ /*  console.log("5 * " + (i) + "=" + (5 * i));
  if( i == 5) {
         break;
 }
 }
+*/
+
+ /* for (let i = 1; i <= 10; i++) {
+    const element = i;
+    console.log(element);
+    if(element == 5) {
+        console.log("5 is best");
+    }
+    
+}  */
+
+ /* (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 2; j++) {
+        console.log(i , j);
+    }
+    console.log(endl);
+}  */
+
+ /* for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop value: ${i} ` );
+    for (let j = 0; j <= 10; j++) {
+       // console.log(`Inner loop value ${j} and outer loop  ${i} `);
+        console.log( i + " *" + j + "=" + (i * j));
+
+    }
+}  */
+
+  /* for (let i = 1; i <= 3; i++) {
+
+    for (let j = 1; j <= 3; j++) {
+
+        console.log(i, j);
+
+    }
+
+} */
