6e2bb52b9e96840b759813f1afcc17a9fcdb5402
Nitin0069 <nitin4rana@gmail.com>
2026-08-07 23:02:06 +0530

    condition in for each statment

--- 05_basics/tut6.js
+++ 05_basics/tut6.js
@@ -1,7 +1,45 @@
- const coding = ["js", "ruby" ,"java" , "python" , "cpp"]
+ //const coding = ["js", "ruby" ,"java" , "python" , "cpp"]
 
- const values =  coding.forEach ( (item) => {
-    console.log(item);
-    return item;
+ //const values =  coding.forEach ( (item) => {
+   //// console.log(item);
+   // return item;
+ //})
+ //console.log(values);
+
+
+ const myNums = [ 1 , 2 , 3, 4, 5 , 6, 7, 8, 9, 10];
+/*
+ const arr = myNums.filter( (nums) => 
+     nums < 5
+ )
+ console.log(arr);
+
+
+  const myNums = [ 1 , 2 , 3, 4, 5 , 6, 7, 8, 9, 10];
+
+ const arr = myNums.filter( (nums) => {
+    return nums < 5
  })
-console.log(values);
+// console.log(arr);
+
+*/
+
+
+function sayHello() {
+    console.log("Hello");
+}
+
+function test(x) {
+    x();
+}
+
+test(sayHello);
+
+
+const newNums = []
+myNums.forEach( (num) =>  {
+    if (num > 4 ) {
+        newNums.push(num)
+    }
+} )
+console.log(newNums);
