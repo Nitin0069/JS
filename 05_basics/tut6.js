388b833a1f9a0b02fb052b4f758b1206fe5f0ec6
Nitin0069 <nitin4rana@gmail.com>
2026-08-07 23:29:04 +0530

    filter finish

--- 05_basics/tut6.js
+++ 05_basics/tut6.js
@@ -26,7 +26,7 @@
 
 
 function sayHello() {
-    console.log("Hello");
+  //  console.log("Hello");
 }
 
 function test(x) {
@@ -41,5 +41,30 @@ myNums.forEach( (num) =>  {
     if (num > 4 ) {
         newNums.push(num)
     }
-} )
-console.log(newNums);
+} )  
+ // console.log(newNums);
+
+
+
+
+
+
+
+
+
+const books = [
+  { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+  { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+  { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+  { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+  { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+  { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+  { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+  { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+];
+
+const obj = books.filter( (arr) => {
+return  arr.publish >= 2000;
+});
+console.log(obj);   
+
