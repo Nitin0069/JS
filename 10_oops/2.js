3447b6e7dc359c1944bb6e89b125cd4b7a20018b
Nitin0069 <nitin4rana@gmail.com>
2026-08-22 11:05:07 +0530

    object literal

---
+++ 10_oops/2.js
@@ -0,0 +1,11 @@
+const user = {
+    name : "nitin",
+    roll : "rana",
+    language : "js",
+
+    getUserDetails: function () {
+        console.log("got user data")
+    }
+}
+console.log(user.roll)
+console.log(user.getUserDetails)
